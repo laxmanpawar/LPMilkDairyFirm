aa4a8fb65d592ee1e0a8d45a4ea19fe717e943c3
Laxman <laxmanpawar1437@gmail.com>
2020-07-01 23:20:05 +0530

    Laxman : Added new form to edit Active Cust List

---
+++ LogInForm/src/LPEditMilkCollectionCustListForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace LogInForm.src
+{
+    partial class LPEditMilkCollectionCustListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_pCloseButton = new System.Windows.Forms.Button();
+            this.m_pResetButton = new System.Windows.Forms.Button();
+            this.m_pSaveButton = new System.Windows.Forms.Button();
+            this.m_pActiveCustListLabel = new System.Windows.Forms.Label();
+            this.m_pTotalCustListLabel = new System.Windows.Forms.Label();
+            this.m_pTotalCustListDGV = new System.Windows.Forms.DataGridView();
+            this.m_pMoveSelectedToLeftButton = new System.Windows.Forms.Button();
+            this.m_pMoveSelectedToRightButton = new System.Windows.Forms.Button();
+            this.m_pMoveAllToLeftButton = new System.Windows.Forms.Button();
+            this.m_pMoveAllToRightButton = new System.Windows.Forms.Button();
+            this.m_pActiveCustListDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.m_pTotalCustListDGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_pActiveCustListDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // m_pCloseButton
+            // 
+            this.m_pCloseButton.Location = new System.Drawing.Point(705, 723);
+            this.m_pCloseButton.Name = "m_pCloseButton";
+            this.m_pCloseButton.Size = new System.Drawing.Size(125, 47);
+            this.m_pCloseButton.TabIndex = 20;
+            this.m_pCloseButton.Text = "Close";
+            this.m_pCloseButton.UseVisualStyleBackColor = true;
+            this.m_pCloseButton.Click += new System.EventHandler(this.m_pCloseButton_Click);
+            // 
+            // m_pResetButton
+            // 
+            this.m_pResetButton.Location = new System.Drawing.Point(551, 723);
+            this.m_pResetButton.Name = "m_pResetButton";
+            this.m_pResetButton.Size = new System.Drawing.Size(125, 47);
+            this.m_pResetButton.TabIndex = 20;
+            this.m_pResetButton.Text = "Reset";
+            this.m_pResetButton.UseVisualStyleBackColor = true;
+            this.m_pResetButton.Click += new System.EventHandler(this.m_pResetButton_Click);
+            // 
+            // m_pSaveButton
+            // 
+            this.m_pSaveButton.Location = new System.Drawing.Point(399, 723);
+            this.m_pSaveButton.Name = "m_pSaveButton";
+            this.m_pSaveButton.Size = new System.Drawing.Size(125, 47);
+            this.m_pSaveButton.TabIndex = 20;
+            this.m_pSaveButton.Text = "Save";
+            this.m_pSaveButton.UseVisualStyleBackColor = true;
+            this.m_pSaveButton.Click += new System.EventHandler(this.m_pSaveButton_Click);
+            // 
+            // m_pActiveCustListLabel
+            // 
+            this.m_pActiveCustListLabel.Font = new System.Drawing.Font("Shivaji01", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_pActiveCustListLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.m_pActiveCustListLabel.Location = new System.Drawing.Point(834, 56);
+            this.m_pActiveCustListLabel.Name = "m_pActiveCustListLabel";
+            this.m_pActiveCustListLabel.Size = new System.Drawing.Size(200, 50);
+            this.m_pActiveCustListLabel.TabIndex = 19;
+            this.m_pActiveCustListLabel.Text = "saMklana yaadI";
+            // 
+            // m_pTotalCustListLabel
+            // 
+            this.m_pTotalCustListLabel.Font = new System.Drawing.Font("Shivaji01", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.m_pTotalCustListLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.m_pTotalCustListLabel.Location = new System.Drawing.Point(186, 56);
+            this.m_pTotalCustListLabel.Name = "m_pTotalCustListLabel";
+            this.m_pTotalCustListLabel.Size = new System.Drawing.Size(200, 50);
+            this.m_pTotalCustListLabel.TabIndex = 19;
+            this.m_pTotalCustListLabel.Text = "]%padk yaadI";
+            // 
+            // m_pTotalCustListDGV
+            // 
+            this.m_pTotalCustListDGV.AllowUserToAddRows = false;
+            this.m_pTotalCustListDGV.AllowUserToDeleteRows = false;
+            this.m_pTotalCustListDGV.AllowUserToResizeRows = false;
+            this.m_pTotalCustListDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.m_pTotalCustListDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.m_pTotalCustListDGV.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.m_pTotalCustListDGV.Location = new System.Drawing.Point(47, 119);
+            this.m_pTotalCustListDGV.Name = "m_pTotalCustListDGV";
+            this.m_pTotalCustListDGV.ReadOnly = true;
+            this.m_pTotalCustListDGV.RowHeadersWidth = 51;
+            this.m_pTotalCustListDGV.RowTemplate.Height = 24;
+            this.m_pTotalCustListDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.m_pTotalCustListDGV.Size = new System.Drawing.Size(529, 580);
+            this.m_pTotalCustListDGV.TabIndex = 18;
+            // 
+            // m_pMoveSelectedToLeftButton
+            // 
+            this.m_pMoveSelectedToLeftButton.Location = new System.Drawing.Point(583, 413);
+            this.m_pMoveSelectedToLeftButton.Name = "m_pMoveSelectedToLeftButton";
+            this.m_pMoveSelectedToLeftButton.Size = new System.Drawing.Size(105, 35);
+            this.m_pMoveSelectedToLeftButton.TabIndex = 17;
+            this.m_pMoveSelectedToLeftButton.Text = "<-Selected";
+            this.m_pMoveSelectedToLeftButton.UseVisualStyleBackColor = true;
+            this.m_pMoveSelectedToLeftButton.Click += new System.EventHandler(this.m_pMoveSelectedToLeftButton_Click);
+            // 
+            // m_pMoveSelectedToRightButton
+            // 
+            this.m_pMoveSelectedToRightButton.Location = new System.Drawing.Point(583, 369);
+            this.m_pMoveSelectedToRightButton.Name = "m_pMoveSelectedToRightButton";
+            this.m_pMoveSelectedToRightButton.Size = new System.Drawing.Size(105, 37);
+            this.m_pMoveSelectedToRightButton.TabIndex = 16;
+            this.m_pMoveSelectedToRightButton.Text = "Selected->";
+            this.m_pMoveSelectedToRightButton.UseVisualStyleBackColor = true;
+            this.m_pMoveSelectedToRightButton.Click += new System.EventHandler(this.m_pMoveSelectedToRightButton_Click);
+            // 
+            // m_pMoveAllToLeftButton
+            // 
+            this.m_pMoveAllToLeftButton.Location = new System.Drawing.Point(583, 322);
+            this.m_pMoveAllToLeftButton.Name = "m_pMoveAllToLeftButton";
+            this.m_pMoveAllToLeftButton.Size = new System.Drawing.Size(105, 40);
+            this.m_pMoveAllToLeftButton.TabIndex = 15;
+            this.m_pMoveAllToLeftButton.Text = "<- All";
+            this.m_pMoveAllToLeftButton.UseVisualStyleBackColor = true;
+            this.m_pMoveAllToLeftButton.Click += new System.EventHandler(this.m_pMoveAllToLeftButton_Click);
+            // 
+            // m_pMoveAllToRightButton
+            // 
+            this.m_pMoveAllToRightButton.Location = new System.Drawing.Point(582, 276);
+            this.m_pMoveAllToRightButton.Name = "m_pMoveAllToRightButton";
+            this.m_pMoveAllToRightButton.Size = new System.Drawing.Size(106, 39);
+            this.m_pMoveAllToRightButton.TabIndex = 14;
+            this.m_pMoveAllToRightButton.Text = "All ->";
+            this.m_pMoveAllToRightButton.UseVisualStyleBackColor = true;
+            this.m_pMoveAllToRightButton.Click += new System.EventHandler(this.m_pMoveAllToRightButton_Click);
+            // 
+            // m_pActiveCustListDGV
+            // 
+            this.m_pActiveCustListDGV.AllowUserToAddRows = false;
+            this.m_pActiveCustListDGV.AllowUserToDeleteRows = false;
+            this.m_pActiveCustListDGV.AllowUserToResizeRows = false;
+            this.m_pActiveCustListDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.m_pActiveCustListDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.m_pActiveCustListDGV.Location = new System.Drawing.Point(694, 119);
+            this.m_pActiveCustListDGV.Name = "m_pActiveCustListDGV";
+            this.m_pActiveCustListDGV.ReadOnly = true;
+            this.m_pActiveCustListDGV.RowHeadersWidth = 51;
+            this.m_pActiveCustListDGV.RowTemplate.Height = 24;
+            this.m_pActiveCustListDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.m_pActiveCustListDGV.Size = new System.Drawing.Size(530, 580);
+            this.m_pActiveCustListDGV.TabIndex = 13;
+            // 
+            // LPEditMilkCollectionCustListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1274, 772);
+            this.Controls.Add(this.m_pCloseButton);
+            this.Controls.Add(this.m_pResetButton);
+            this.Controls.Add(this.m_pSaveButton);
+            this.Controls.Add(this.m_pActiveCustListLabel);
+            this.Controls.Add(this.m_pTotalCustListLabel);
+            this.Controls.Add(this.m_pTotalCustListDGV);
+            this.Controls.Add(this.m_pMoveSelectedToLeftButton);
+            this.Controls.Add(this.m_pMoveSelectedToRightButton);
+            this.Controls.Add(this.m_pMoveAllToLeftButton);
+            this.Controls.Add(this.m_pMoveAllToRightButton);
+            this.Controls.Add(this.m_pActiveCustListDGV);
+            this.Name = "LPEditMilkCollectionCustListForm";
+            this.Text = "LPEditMilkCollectionCustListForm";
+            this.Load += new System.EventHandler(this.LPEditMilkCollectionCustListForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.m_pTotalCustListDGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m_pActiveCustListDGV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView m_pTotalCustListDGV;
+        private System.Windows.Forms.Button m_pMoveSelectedToLeftButton;
+        private System.Windows.Forms.Button m_pMoveSelectedToRightButton;
+        private System.Windows.Forms.Button m_pMoveAllToLeftButton;
+        private System.Windows.Forms.Button m_pMoveAllToRightButton;
+        private System.Windows.Forms.DataGridView m_pActiveCustListDGV;
+        private System.Windows.Forms.Label m_pTotalCustListLabel;
+        private System.Windows.Forms.Label m_pActiveCustListLabel;
+        private System.Windows.Forms.Button m_pSaveButton;
+        private System.Windows.Forms.Button m_pResetButton;
+        private System.Windows.Forms.Button m_pCloseButton;
+    }
+}
---
+++ LogInForm/src/LPEditMilkCollectionCustListForm.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LogInForm.src
+{
+    public partial class LPEditMilkCollectionCustListForm : src.LPForm
+    {
+        List<int> m_pActiveCustList = new List<int>();
+        List<int> m_pOrigActiveCustList = new List<int>();
+        DataTable m_pTotalCustListTable = new DataTable();
+        DataTable m_pActiveCustListTable = new DataTable();
+
+        public LPEditMilkCollectionCustListForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LPEditMilkCollectionCustListForm_Load(object sender, EventArgs e)
+        {
+            // Update Total List Cust DataGridView
+            SqlConnection con = new SqlConnection(LPSQLTableUtils.m_sSqlConnectionString);
+            string query = "SELECT * FROM " + LPGlobalVariables.m_sTotalCustomerDataTable;
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            sda.Fill(m_pTotalCustListTable);
+            m_pTotalCustListDGV.DataSource = m_pTotalCustListTable;
+
+            this.m_pTotalCustListDGV.Columns["CUST_CODE"].Width = 60;
+            this.m_pTotalCustListDGV.Columns["CUST_CODE"].HeaderText = "Code";
+            this.m_pTotalCustListDGV.Columns["CUST_NAME"].HeaderText = "Name";
+            this.m_pTotalCustListDGV.Columns["CUST_NAME"].DefaultCellStyle.Font = new System.Drawing.Font("Shivaji01", 18F, FontStyle.Bold);
+
+            // Remove Unnecessary Columns from DataGridView
+            this.m_pTotalCustListDGV.Columns.Remove("MILK_TYPE");
+            this.m_pTotalCustListDGV.Columns.Remove("CUST_MOB");
+            this.m_pTotalCustListDGV.Columns.Remove("CUST_ACC_NO");
+
+            cmd.CommandText = "SELECT * FROM " + LPGlobalVariables.m_sCollectionCustomerDataTable;
+            SqlDataAdapter sda1 = new SqlDataAdapter(cmd);
+            sda.Fill(m_pActiveCustListTable);
+            m_pActiveCustListDGV.DataSource = m_pActiveCustListTable;
+
+            // Fill Active Cust List.
+            m_pActiveCustList = m_pActiveCustListTable.AsEnumerable().Select(r => r.Field<int>(0)).ToList();
+            
+            m_pOrigActiveCustList.AddRange(m_pActiveCustList);
+
+            this.m_pActiveCustListDGV.Columns["CUST_CODE"].Width = 60;
+            this.m_pActiveCustListDGV.Columns["CUST_CODE"].HeaderText = "Code";
+            this.m_pActiveCustListDGV.Columns["CUST_NAME"].HeaderText = "Name";
+            this.m_pActiveCustListDGV.Columns["CUST_NAME"].DefaultCellStyle.Font = new System.Drawing.Font("Shivaji01", 18F, FontStyle.Bold);
+
+            // Remove Unnecessary Columns from DataGridView
+            this.m_pActiveCustListDGV.Columns.Remove("MILK_TYPE");
+            this.m_pActiveCustListDGV.Columns.Remove("CUST_MOB");
+            this.m_pActiveCustListDGV.Columns.Remove("CUST_ACC_NO");
+        }
+
+        private void m_pMoveAllToRightButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow dgvRow in this.m_pTotalCustListDGV.Rows)
+                {
+                    if (!m_pActiveCustList.Contains(Convert.ToInt32(dgvRow.Cells[0].Value)))
+                    {
+                        // Add Row 
+                        DataRow dataRow = m_pActiveCustListTable.NewRow(); //.Rows.Add(dgvRow.Cells[0].Value, dgvRow.Cells[1].Value);
+                        dataRow["CUST_CODE"] = dgvRow.Cells[0].Value;
+                        dataRow["CUST_NAME"] = dgvRow.Cells[1].Value;
+                        m_pActiveCustListTable.Rows.Add(dataRow);
+                        m_pActiveCustListTable.AcceptChanges();
+
+                        m_pActiveCustList.Add(Convert.ToInt32(dgvRow.Cells[0].Value));
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("LPError : " + exc.Message, "LPError", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void m_pMoveAllToLeftButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                m_pActiveCustListTable.Rows.Clear();
+                m_pActiveCustList.Clear();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("LPError : " + exc.Message, "LPError", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void m_pMoveSelectedToRightButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow dgvRow in this.m_pTotalCustListDGV.SelectedRows)
+                {
+                    if (!m_pActiveCustList.Contains(Convert.ToInt32(dgvRow.Cells[0].Value)))
+                    {
+                        // Add Row 
+                        DataRow dataRow = m_pActiveCustListTable.NewRow(); //.Rows.Add(dgvRow.Cells[0].Value, dgvRow.Cells[1].Value);
+                        dataRow["CUST_CODE"] = dgvRow.Cells[0].Value;
+                        dataRow["CUST_NAME"] = dgvRow.Cells[1].Value;
+                        DataRow totCustDataRow = m_pTotalCustListTable.Select("CUST_CODE = " + dgvRow.Cells[0].Value).First();
+                        dataRow["MILK_TYPE"] = totCustDataRow.Field<int>("MILK_TYPE");
+                        dataRow["CUST_MOB"] = totCustDataRow.Field<Int64>("CUST_MOB");
+                        dataRow["CUST_ACC_NO"] = totCustDataRow.Field<Int64>("CUST_ACC_NO");
+
+                        m_pActiveCustListTable.Rows.Add(dataRow);
+                        m_pActiveCustListTable.AcceptChanges();
+
+                        m_pActiveCustList.Add(Convert.ToInt32(dgvRow.Cells[0].Value));
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("LPError : " + exc.Message, "LPError", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void m_pMoveSelectedToLeftButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (DataGridViewRow dgvRow in this.m_pActiveCustListDGV.SelectedRows)
+                {
+                    m_pActiveCustList.Remove(Convert.ToInt32(dgvRow.Cells[0].Value));
+                    this.m_pActiveCustListDGV.Rows.Remove(dgvRow);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("LPError : " + exc.Message, "LPError", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void m_pSaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Add Rows to SQL Table\
+                List<string> columnNames = new List<string>();
+                columnNames.Add("CUST_CODE");
+                columnNames.Add("CUST_NAME");
+                columnNames.Add("MILK_TYPE");
+                columnNames.Add("CUST_MOB");
+                columnNames.Add("CUST_ACC_NO");
+
+                SqlConnection con = new SqlConnection(LPSQLTableUtils.m_sSqlConnectionString);
+
+                StringBuilder query = new StringBuilder();
+                query.Append("INSERT INTO " + LPGlobalVariables.m_sCollectionCustomerDataTable + " VALUES");
+
+                List<int> newActiveCustList = new List<int>();
+
+                int rowCounter = 0;
+                foreach (DataRow dr in m_pActiveCustListTable.Rows)
+                {
+                    ++rowCounter;
+                    Object[] values = dr.ItemArray;
+
+                    if (m_pOrigActiveCustList.Contains(Convert.ToInt32(values[0]))) continue;
+
+                    newActiveCustList.Add(Convert.ToInt32(values[0]));
+
+                    query.Append("(" + values[0]);
+                    query.Append(", '" + values[1] + "'");
+                    query.Append(", " + values[2].ToString());
+                    query.Append(", " + values[3].ToString());
+                    query.Append(", " + values[4].ToString());
+
+                    if (rowCounter == m_pActiveCustListTable.Rows.Count)
+                    {
+                        query.Append(")");
+                    }
+                    else
+                    {
+                        query.Append("),");
+                    }
+                }
+
+                /*int rowCounter = 0;
+                foreach (DataGridViewRow dgvr in m_pActiveCustListTable.Rows)
+                {
+                    if (rowCounter == m_pActiveCustListTable.Rows.Count) continue;
+                    for (int i = 0; i < columnNames.Count; ++i)
+                    {
+                        string value = dgvr.Cells[i].Value.ToString();
+                        if (i % columnNames.Count == 0) query.Append("(" + value);
+                        else if (columnNames[i] == "CUST_NAME") query.Append(", '" + value + "'");
+                        else if (columnNames[i] == "MILK_TYPE" && value == "gaaya") query.Append(", " + 0.ToString());
+                        else if (columnNames[i] == "MILK_TYPE" && value == "mhOsa") query.Append(", " + 1.ToString());
+                        else query.Append(", " + value.ToString());
+                    }
+                    ++rowCounter;
+                    if (rowCounter == m_pActiveCustListTable.Rows.Count) query.Append(")");
+                    else query.Append("),");
+                }*/
+
+                SqlCommand cmd = new SqlCommand(query.ToString(), con);
+
+                con.Open();
+
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    m_pOrigActiveCustList.AddRange(newActiveCustList);
+                    MessageBox.Show("LPInfo : Active Customer List  is updated Successfully.", "LPINFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else MessageBox.Show("LPInfo : Active Customer List is upadation failed.", "LPError", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                con.Close();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("LPError : " + exc.Message, "LPError", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("LPInfo : Active Customer List is upadation failed.", "LPError", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void m_pResetButton_Click(object sender, EventArgs e)
+        {
+            m_pActiveCustListDGV.Rows.Clear();
+        }
+
+        private void m_pCloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
